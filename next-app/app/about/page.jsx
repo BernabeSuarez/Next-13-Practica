83737281d55bc9b1a2800f8ce4d75299dc0ed475
Bernabe <bernatattoo@gmail.com>
2022-12-30 02:35:46 -0300

    First commit Next 13 practicas

---
+++ next-app/app/about/page.jsx
@@ -0,0 +1,3 @@
+export default function AboutPage() {
+  return <h1>Esto es Sobre Nosotros...</h1>;
+}
---
+++ next-app/app/components/Header.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import styles from "./Header.module.css";
+
+const links = [
+  {
+    label: "Home",
+    route: "/",
+  },
+  {
+    label: "About",
+    route: "/about",
+  },
+];
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul>
+          {links.map(({ label, route }) => (
+            <li key={label}>
+              <Link href={route}>{label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
